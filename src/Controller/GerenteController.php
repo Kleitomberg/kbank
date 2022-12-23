ae41fa7666310093c628b00250c97dba4bdb6c71
kleitomberg <kleitom-1@hotmail.com>
2022-12-22 23:06:24 -0300

    transações gerente

--- src/Controller/GerenteController.php
+++ src/Controller/GerenteController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Gerente;
+use App\Entity\Transacao;
 use App\Entity\User;
 use App\Repository\AgenciaRepository;
 use App\Repository\ContaRepository;
@@ -13,6 +14,7 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class GerenteController extends AbstractController
@@ -101,4 +103,224 @@ class GerenteController extends AbstractController
 
 
     }
+
+    #editar agencia
+    #[Route('/gerente/editarAgencia/{agencia}', name: 'app_agencia_editar')]
+    public function editarAgencia(Request $request, UserRepository $userRepository, $agencia, AgenciaRepository $agenciaRepository, EntityManagerInterface $entityManager): Response
+    {
+        
+
+
+
+       if ($request->isMethod('POST')){
+        $agencia = $agenciaRepository->find($agencia);
+        
+        $agencia->setCodigo($request->request->get('codigo'));
+        $agencia->setBairro($request->request->get('bairro'));
+        $agencia->setCep($request->request->get('cep'));
+        $agencia->setCidade($request->request->get('cidade'));
+        $agencia->setEstado($request->request->get('estado'));
+        $agencia->setNumero($request->request->get('numero'));
+        $agencia->setRua($request->request->get('rua'));
+        $agencia->setTelefone($request->request->get('telefone'));
+       
+
+        $entityManager->persist($agencia);
+        $entityManager->flush();
+
+        $gerente = $this->getUser();
+        $gerente->getUserIdentifier();
+
+        $gerente = $userRepository->findOneBy(['email' => $gerente->getUserIdentifier()]);
+
+        $this->addFlash('success', 'Agência editada com sucesso!');
+        return $this->redirectToRoute('app_gerente', ['gerente' => $gerente->getId()]);
+       }
+       return $this->render('gerente/form_agencia.twig', [
+        'controller_name' => 'GerenteController',
+        'agencia' => $agenciaRepository->find($agencia),
+    ]);
+
+
+        
+    }
+    #editar cliente
+    #[Route('/gerente/editarCliente/{cliente}', name: 'app_cliente_editar')]
+    public function editarCliente(Request $request, UserPasswordHasherInterface $userPasswordHasherInterface, UserRepository $userRepository, $cliente, EntityManagerInterface $entityManager): Response
+    {
+        $user = $userRepository->find($cliente);
+        
+        if ($request->isMethod('POST')){
+            $user->setNome($request->request->get('nome'));
+            $user->setCpf($request->request->get('cpf'));
+            $user->setEmail($request->request->get('email'));
+            $user->setCelular($request->request->get('celular'));
+            $user->setPassword(
+                $userPasswordHasherInterface->hashPassword(
+                    $user,
+                    $request->request->get('password')
+                )
+
+                );
+
+           
+            $user->setIsVerified(true);
+            
+            $entityManager->persist($user);
+            $entityManager->flush();
+    
+            $gerente = $this->getUser();
+            $gerente->getUserIdentifier();
+    
+            $gerente = $userRepository->findOneBy(['email' => $gerente->getUserIdentifier()]);
+    
+            $this->addFlash('success', 'Cliente editado com sucesso!');
+            return $this->redirectToRoute('app_gerente', ['gerente' => $gerente->getId()]);
+           }
+
+           return $this->render('gerente/form_cliente.twig', [
+            'controller_name' => 'GerenteController',
+            'cliente' => $user,
+        ]);
+
+        
+
+    }
+
+    #acessar conta
+    #[Route('/gerente/{gerente}/acessarConta/{conta}', name: 'app_conta_acessar_gerente')]
+    public function acessarConta(Request $request, $conta, ContaRepository $contaRepository, TransacaoRepository $transacaoRepository): Response
+    {
+        $conta = $contaRepository->find($conta);
+        $transacoes = $transacaoRepository->findBy(['destinatario' => $conta]);
+        $transacoes_b = $transacaoRepository->findBy(['remetente' => $conta]);
+
+        $transacoes = array_merge($transacoes, $transacoes_b);
+
+        return $this->render('gerente/conta.html.twig', [
+            'controller_name' => 'GerenteController',
+            'conta' => $conta,
+            'transacoes' => $transacoes,
+        ]);
+    }
+
+    #transações
+
+    #sacar
+    #[Route('/gerente/{gerente}/acessarConta/{conta}/sacar', name: 'app_conta_sacar_gerente')]
+    public function sacar(Request $request, $conta, $gerente,UserRepository $userRepository, ContaRepository $contaRepository, TransacaoRepository $transacaoRepository, EntityManagerInterface $entityManager): Response
+    {   
+        $user = $this->getUser();
+        $user->getUserIdentifier();
+        $gerente = $userRepository->findOneBy(['email' => $user->getUserIdentifier()]);
+
+        
+        $conta = $contaRepository->find($conta);
+        $transacoes = $transacaoRepository->findBy(['destinatario' => $conta]);
+        $transacoes_b = $transacaoRepository->findBy(['remetente' => $conta]);
+
+        $transacoes = array_merge($transacoes, $transacoes_b);
+
+        if ($request->isMethod('POST')){
+            $valor = $request->request->get('valor');
+            $conta->setSaldo($conta->getSaldo() - $valor);
+            $transacao = new Transacao();
+            $transacao->setValor($valor);
+            $transacao->setDestinatario($conta);
+            $transacao->setDescricao('Saque');
+            $transacao->setData(new \DateTime());
+            $entityManager->persist($transacao);
+            $entityManager->persist($conta);
+            $entityManager->flush();
+            $this->addFlash('success', 'Saque realizado com sucesso!');
+            return $this->redirectToRoute('app_conta_acessar_gerente', ['gerente'=> $gerente->getId() ,'conta' => $conta->getId()]);
+        }
+
+        return $this->render('gerente/conta.html.twig', [
+            'controller_name' => 'GerenteController',
+            'conta' => $conta,
+            'transacoes' => $transacoes,
+        ]);
+    }
+
+    #depositar
+
+    #[Route('/gerente/{gerente}/acessarConta/{conta}/depositar', name: 'app_conta_depositar_gerente')]
+    public function depositar(Request $request, $conta, $gerente,UserRepository $userRepository, ContaRepository $contaRepository, TransacaoRepository $transacaoRepository, EntityManagerInterface $entityManager): Response
+    {   
+        $user = $this->getUser();
+        $user->getUserIdentifier();
+        $gerente = $userRepository->findOneBy(['email' => $user->getUserIdentifier()]);
+
+        
+        $conta = $contaRepository->find($conta);
+        $transacoes = $transacaoRepository->findBy(['destinatario' => $conta]);
+        $transacoes_b = $transacaoRepository->findBy(['remetente' => $conta]);
+
+        $transacoes = array_merge($transacoes, $transacoes_b);
+
+        if ($request->isMethod('POST')){
+            $valor = $request->request->get('valor');
+            $conta->setSaldo($conta->getSaldo() + $valor);
+            $transacao = new Transacao();
+            $transacao->setValor($valor);
+            $transacao->setDestinatario($conta);
+            $transacao->setDescricao('Depósito');
+            $transacao->setData(new \DateTime());
+            $entityManager->persist($transacao);
+            $entityManager->persist($conta);
+            $entityManager->flush();
+            $this->addFlash('success', 'Depósito realizado com sucesso!');
+            return $this->redirectToRoute('app_conta_acessar_gerente', ['gerente'=> $gerente->getId() ,'conta' => $conta->getId()]);
+        }
+
+        return $this->render('gerente/conta.html.twig', [
+            'controller_name' => 'GerenteController',
+            'conta' => $conta,
+            'transacoes' => $transacoes,
+        ]);
+    }
+
+    #transferir
+
+    #[Route('/gerente/{gerente}/acessarConta/{conta}/transferir', name: 'app_conta_transferir_gerente')]
+    public function transferir(Request $request, $conta, $gerente,UserRepository $userRepository, ContaRepository $contaRepository, TransacaoRepository $transacaoRepository, EntityManagerInterface $entityManager): Response
+    {   
+        $user = $this->getUser();
+        $user->getUserIdentifier();
+        $gerente = $userRepository->findOneBy(['email' => $user->getUserIdentifier()]);
+
+        
+        $conta = $contaRepository->find($conta);
+
+        
+        $transacoes = $transacaoRepository->findBy(['destinatario' => $conta]);
+        $transacoes_b = $transacaoRepository->findBy(['remetente' => $conta]);
+
+        $transacoes = array_merge($transacoes, $transacoes_b);
+
+        if ($request->isMethod('POST')){
+            $valor = $request->request->get('valor');
+            $transacao = new Transacao();
+            $transacao->setValor($valor);
+            $transacao->setRemetente($conta);
+            $conta_destino = $contaRepository->findOneBy(['numero' => $request->request->get('conta')]);
+            $transacao->setDestinatario($conta_destino);
+            $conta->transferir($valor,$conta_destino);
+            $transacao->setDescricao('Transferência');
+            $transacao->setData(new \DateTime());
+            $entityManager->persist($transacao);
+            $entityManager->persist($conta);
+            $entityManager->flush();
+            $this->addFlash('success', 'Transferência realizada com sucesso!');
+            return $this->redirectToRoute('app_conta_acessar_gerente', ['gerente'=> $gerente->getId() ,'conta' => $conta->getId()]);
+        }
+
+        return $this->render('gerente/conta.html.twig', [
+            'controller_name' => 'GerenteController',
+            'conta' => $conta,
+            'transacoes' => $transacoes,
+        ]);
+    }
+
 }
